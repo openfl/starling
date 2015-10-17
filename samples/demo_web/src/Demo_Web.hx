d46e4bfe7823ce180ee9e9b1c355b8a787b9d417
vroad <xvroad@gmail.com>
2015-10-18 00:56:01 +0900

    Fixes for android build - Resize handling, full DCE, disabling stencil buffer, loading without URLLoader, etc

--- samples/demo_web/src/Demo_Web.hx
+++ samples/demo_web/src/Demo_Web.hx
@@ -6,11 +6,20 @@ import flash.system.Capabilities;
 import flash.utils.setTimeout;
 #end
 import haxe.Timer;
+import openfl.Assets;
 import openfl.display3D.Context3DRenderMode;
+import openfl.errors.Error;
+import openfl.geom.Rectangle;
+import starling.display.Stage;
+import starling.text.BitmapFont;
+import starling.text.TextField;
+import starling.textures.Texture;
+import starling.textures.TextureAtlas;
+import starling.utils.Max;
+import starling.utils.RectangleUtil;
 
 import starling.core.Starling;
 import starling.events.Event;
-import starling.textures.RenderTexture;
 import starling.utils.AssetManager;
 
 import utils.ProgressBar;
@@ -56,12 +65,16 @@ class Demo_Web extends Sprite
         RenderTexture.optimizePersistentBuffers = true; // should be safe on Desktop
 
         mStarling = new Starling(Game, stage, null, null, Context3DRenderMode.AUTO, "auto");
+        mStarling.stage.stageWidth = Constants.GameWidth;
+        mStarling.stage.stageHeight = Constants.GameHeight;
         mStarling.simulateMultitouch = true;
         mStarling.enableErrorChecking = Capabilities.isDebugger;
         mStarling.addEventListener(Event.ROOT_CREATED, function():Void
         {
             loadAssets(startGame);
         });
+        
+        this.stage.addEventListener(Event.RESIZE, onResize, false, Max.INT_MAX_VALUE, true);
 
         mStarling.start();
         initElements();
@@ -77,7 +90,7 @@ class Demo_Web extends Sprite
         assets.verbose = Capabilities.isDebugger;
         #if 0
         assets.enqueue(EmbeddedAssets);
-        #else
+        #elseif 0
         assets.enqueue(
         [
             "assets/textures/1x/atlas.xml",
@@ -90,12 +103,23 @@ class Demo_Web extends Sprite
             "assets/fonts/1x/desyrel.png",
             "assets/audio/wing_flap.ogg"
         ]);
+        #else
+        var atlasTexture:Texture = Texture.fromBitmapData(Assets.getBitmapData("assets/textures/1x/atlas.png"), false);
+        var atlasXml:Xml = Xml.parse(Assets.getText("assets/textures/1x/atlas.xml")).firstElement();
+        var desyrelTexture:Texture = Texture.fromBitmapData(Assets.getBitmapData("assets/fonts/1x/desyrel.png"), false);
+        var desyrelXml:Xml = Xml.parse(Assets.getText("assets/fonts/1x/desyrel.fnt")).firstElement();
+        TextField.registerBitmapFont(new BitmapFont(desyrelTexture, desyrelXml));
+        assets.addTexture("atlas", atlasTexture);
+        assets.addTextureAtlas("atlas", new TextureAtlas(atlasTexture, atlasXml));
+        assets.addTexture("background", Texture.fromBitmapData(Assets.getBitmapData("assets/textures/1x/background.jpg"), false));
+        assets.addSound("wing_flap", Assets.getSound("assets/audio/wing_flap.ogg"));
         #end
 
         // Now, while the AssetManager now contains pointers to all the assets, it actually
         // has not loaded them yet. This happens in the "loadQueue" method; and since this
         // will take a while, we'll update the progress bar accordingly.
 
+        #if 0
         assets.loadQueue(function(ratio:Float):Void
         {
             #if 0
@@ -103,6 +127,9 @@ class Demo_Web extends Sprite
             #end
             if (ratio == 1) onComplete(assets);
         });
+        #else
+        Timer.delay(function() { onComplete(assets); }, 1);
+        #end
     }
 
     private function startGame(assets:AssetManager):Void
@@ -143,5 +170,15 @@ class Demo_Web extends Sprite
             removeChild(mProgressBar);
             mProgressBar = null;
         }
+    }
+    
+    private function onResize(e:openfl.events.Event):Void
+    {
+        var viewPort:Rectangle = RectangleUtil.fit(new Rectangle(0, 0, Constants.GameWidth, Constants.GameHeight), new Rectangle(0, 0, stage.stageWidth, stage.stageHeight));
+        try
+        {
+            this.mStarling.viewPort = viewPort;
+        }
+        catch(error:Error) {}
     }
 }
