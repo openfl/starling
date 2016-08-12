979af9734c22a9797959123330a918593338fe34
Joshua Granick <jgranick@users.noreply.github.com>
2016-08-12 16:01:28 -0700

    Fix StringUtil

--- starling/utils/StringUtil.hx
+++ starling/utils/StringUtil.hx
@@ -20,7 +20,7 @@ public static function formatString(format:String, args:Array<Dynamic>):String
     for (i in 0 ... args.length)
 	{
 		var r:EReg = new EReg("\\{" + i + "\\}", "g");
-        format = r.replace(format, args[i]);
+        format = r.replace(format, Std.string (args[i]));
 	}
     
     return format;
