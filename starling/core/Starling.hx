0890eba6dc283b61f1743de1c437caab1cb8409f
Joshua Granick <jgranick@users.noreply.github.com>
2016-09-28 09:29:31 -0700

    Fix header

--- starling/core/Starling.hx
+++ starling/core/Starling.hx
@@ -2,7 +2,7 @@
 //
 //	Starling Framework
 //	Copyright Gamua GmbH. All Rights Reserved.
-//BASELINE_CONSTRAINED
+//
 //	This program is free software. You can redistribute and/or modify it
 //	in accordance with the terms of the accompanying license agreement.
 //
