602830a8e975832def55a00d4a67520120bfe38b
P.J.Shand <pete.shand@imagination.com>
2017-12-19 11:23:26 +1100

    -

--- starling/utils/AssetManager.hx
+++ starling/utils/AssetManager.hx
@@ -929,7 +929,7 @@ class AssetManager extends EventDispatcher
                         dispatchEventWith(Event.PARSE_ERROR, false, name);
                     }
 
-                    if (object) addObject(name, object);
+                    if (object != null) addObject(name, object);
 
                     bytes.clear();
                     onComplete();
