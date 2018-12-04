4f64f78241feafcc5443997bd2dd1d9ab058c295
Joshua Granick <jgranick@users.noreply.github.com>
2018-12-04 15:43:23 -0800

    Avoid Vector<Dynamic>

--- src/starling/events/EventDispatcher.hx
+++ src/starling/events/EventDispatcher.hx
@@ -61,7 +61,7 @@ class EventDispatcher
         if (__eventListeners == null)
             __eventListeners = new Map<String, Vector<Function>>();
         
-        var listeners:Vector<Dynamic> = __eventListeners[type];
+        var listeners:Vector<Function> = __eventListeners[type];
         if (listeners == null)
         {
             __eventListeners[type] = new Vector<Function>();
