2af8d3b9fbe0c10ee254eb6950f9b2636c9dcc55
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-09-28 16:03:08 -0700

    update build to prepare for openfl-js 9.3.0 update

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,27 @@
 const webpack = require ("webpack");
-const merge = require ("webpack-merge");
-const UglifyJSPlugin = require ("uglifyjs-webpack-plugin");
+const { merge } = require ("webpack-merge");
+const BrotliPlugin = require ('brotli-webpack-plugin');
+const CompressionPlugin = require ("compression-webpack-plugin");
 const common = require ("./webpack.common.js");
 const package = require ("./package.json");
 
 module.exports = merge (common, {
+	mode: "production",
 	output: {
 		filename: "starling.min.js"
 	},
 	plugins: [
-		new UglifyJSPlugin ({
-			sourceMap: true,
-			uglifyOptions: {
-				// warnings: true
-			}
-		}),
 		new webpack.BannerPlugin ({
 			banner: "/*! Starling Framework v" + package.version + " | Simplified BSD (c) Gamua | gamua.com/starling */",
 			raw: true,
 			entryOnly: true
 		}),
-		new webpack.DefinePlugin ({
-			"process.env.NODE_ENV": JSON.stringify ("production")
+		new CompressionPlugin ({
+			include: /[A-Za-z0-9]*\.min\.js$/
+		}),
+		new BrotliPlugin ({
+			asset: '[path].br[query]',
+			test: /[A-Za-z0-9]*\.min\.js$/
 		})
 	]
 });
