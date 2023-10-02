8659867f4f838111936c55b5bcfab87c34e37100
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-10-02 14:54:22 -0700

    update demo_npm with new dependencies

--- samples/demo_npm/es6/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require ('webpack-merge');
-const common = require ('./webpack.common.js');
-
-module.exports = merge (common, {
-	devServer: {
-		contentBase: './dist'
-	},
-	devtool: "inline-source-map",
-});
