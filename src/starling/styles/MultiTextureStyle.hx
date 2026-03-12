ad0c28f5d3c86b0d15abedcd20813ed31702ca18
Mathieu Sénidre <matse@skwatt.com>
2026-03-12 14:51:45 +0100

    MultiTextureStyle : fixed issues + optimized + allow setting maxTextures before Starling is started

--- src/starling/styles/MultiTextureStyle.hx
+++ src/starling/styles/MultiTextureStyle.hx
@@ -13,6 +13,7 @@ import openfl.Vector;
 import openfl.display3D.Context3D;
 import openfl.display3D.Context3DProfile;
 import openfl.display3D.Context3DProgramType;
+import openfl.errors.Error;
 import openfl.geom.Matrix;
 import starling.core.Starling;
 import starling.display.Mesh;
@@ -26,17 +27,27 @@ import starling.styles.MeshStyle;
 import starling.textures.Texture;
 import starling.utils.RenderUtil;
 
-/** Provides a way to batch up to 4 different textures in one draw call, at the cost of more complex custom Fragment Shaders
- *  To use this, set Mesh.defaultStyle to MultiTextureStyle (ideally before Starling is initialised!)
- **/
+/**
+   Provides a way to batch up to 5 (baseline profiles) or 16 different textures in one draw call, at the cost of more complex custom Fragment Shaders
+   To use this, set Mesh.defaultStyle to MultiTextureStyle (ideally before Starling is initialised!)
+**/
 class MultiTextureStyle extends MeshStyle 
 {
 	/** The vertex format expected by this style. */
 	public static var VERTEX_FORMAT:VertexDataFormat =
 		MeshStyle.VERTEX_FORMAT.extend("texture:float1");
 	
-	/** Maximum number of textures that can be batched. */
-	public static var MAX_NUM_TEXTURES:Int = 5;
+	/**
+	   Maximum number of textures that can be batched.
+	   Default value is 5 (which is the absolute max with baseline profile)
+	   until <code>MultiTextureStyle.init()</code> has been called with Starling started
+	   You can call it manually if you want/need, otherwise it will be called by the
+	   first MultiTextureStyle instance created.
+	**/
+	public static var MAX_NUM_TEXTURES(get, never):Int;
+	
+	private static var __MAX_NUM_TEXTURES:Int = 5;
+	private static function get_MAX_NUM_TEXTURES():Int { return __MAX_NUM_TEXTURES; }
 	
 	private var __dirty:Bool = true;
 	private var __textures:Vector<Texture> = new Vector<Texture>();
@@ -44,18 +55,61 @@ class MultiTextureStyle extends MeshStyle
 	private static var sMaxTextures:Int = 2;
 	private static var sTextureIndexMap:Array<Int> = new Array<Int>();
 	
+	/**
+	   Maximum number of textures to be batched, default 2.
+	**/
 	public static var maxTextures(get, set):Int;
 	private static function get_maxTextures():Int { return sMaxTextures; }
 	private static function set_maxTextures(value:Int):Int
 	{
-		value = value < 1 ? 1 : value;
-		sMaxTextures = value > MAX_NUM_TEXTURES ? MAX_NUM_TEXTURES : value;
+		if (!__initDone) init();
+		
+		if (!__initDone)
+		{
+			// we don't know the profile yet, allow a max value of 16 (absolute max on non-baseline profile)
+			// that number might be reduced when we can finally check profile
+			value = value < 1 ? 1 : value > 16 ? 16 : value;
+			sMaxTextures = value;
+		}
+		else
+		{
+			value = value < 1 ? 1 : value;
+			sMaxTextures = value > __MAX_NUM_TEXTURES ? __MAX_NUM_TEXTURES : value;
+		}
 		return value;
+	}
+	
+	private static var __TEXTURE_INDEX_FACTOR:Float;
+	
+	private static var __initDone:Bool = false;
+	public static function init():Void
+	{
+		if (__initDone) return;
+		if (Starling.current == null) return;
+		
+		var isBaseline:Bool = Starling.current.profile == Context3DProfile.BASELINE ||
+							  Starling.current.profile == Context3DProfile.BASELINE_CONSTRAINED ||
+							  Starling.current.profile == Context3DProfile.BASELINE_EXTENDED;
+					   
+		if (isBaseline)
+		{
+			__MAX_NUM_TEXTURES = 5;
+			__TEXTURE_INDEX_FACTOR = 4.0;
+		}
+		else
+		{
+			__MAX_NUM_TEXTURES = 16;
+			__TEXTURE_INDEX_FACTOR = 1.0;
+		}
+		
+		__initDone = true;
 	}
 
 	public function new() 
 	{
 		super();
+		
+		if (!__initDone) init();
 	}
 	
 	/** @private */
@@ -106,7 +160,7 @@ class MultiTextureStyle extends MeshStyle
 					{
 						var numSharedTextures:Int = 0;
 						
-						for (i in 0...numSharedTextures)
+						for (i in 0...numTexturesToAdd)
 						{
 							if (mtStyle.getTextureIndex(getTexture(i)) != -1)
 								numSharedTextures ++;
@@ -128,12 +182,12 @@ class MultiTextureStyle extends MeshStyle
 											 matrix:Matrix = null, vertexID:Int = 0,
 											 numVertices:Int = -1):Void 
 	{
-		var length:Int;
+		var count:Int;
 		
 		if (matrix != null && __dirty)
 		{
-			length = vertexData.numVertices;
-			for (i in 0...length)
+			count = vertexData.numVertices;
+			for (i in 0...count)
 				vertexData.setFloat(i, "texture", 0);
 			__dirty = false;
 		}
@@ -146,8 +200,8 @@ class MultiTextureStyle extends MeshStyle
 		{
 			var dirty:Bool = false;
 			
-			length = numTextures;
-			for (i in 0...length)
+			count = numTextures;
+			for (i in 0...count)
 			{
 				var texture:Texture = getTexture(i);
 				var textureIndexOnTarget:Int = mtTarget.getTextureIndex(texture);
@@ -171,11 +225,11 @@ class MultiTextureStyle extends MeshStyle
 				for (i in 0...numVertices)
 				{
 					var sourceTexID:Int = Math.round(targetVertexData.getFloat(targetVertexID + i,
-						"texture") * 4);
+						"texture") * __TEXTURE_INDEX_FACTOR);
 					var targetTexID:Int = sTextureIndexMap[sourceTexID];
 					
 					if (sourceTexID != targetTexID)
-						targetVertexData.setFloat(targetVertexID + i, "texture", targetTexID / 4);
+						targetVertexData.setFloat(targetVertexID + i, "texture", targetTexID / __TEXTURE_INDEX_FACTOR);
 				}
 			}
 		}
@@ -197,20 +251,21 @@ class MultiTextureStyle extends MeshStyle
 	// in the list.
 	private function getTextureIndex(texture:Texture):Int
 	{
-		var length:Int = numTextures;
+		if (_textureRoot == texture) return 0;
+		var length:Int = __textures.length;
 		for (i in 0...length)
-			if (getTexture(i).root == texture.root) return i;
+			if (__textures[i] == texture) return i + 1;
 		return -1;
 	}
 	
 	// Returns an element of the shared texture list.
 	inline private function getTexture(index:Int):Texture
 	{
-		return index > 0 ? __textures[index - 1] : texture;
+		return index > 0 ? __textures[index - 1] : _textureRoot;
 	}
 	
 	public var numTextures(get, never):Int;
-	private function get_numTextures():Int { return texture != null ? __textures.length + 1 : __textures.length; }
+	private function get_numTextures():Int { return _texture != null ? __textures.length + 1 : __textures.length; }
 	
 }
 
@@ -222,10 +277,19 @@ class MultiTextureEffect extends MeshEffect
 	
 	private var __isBaseline:Bool;
 	
-	private static var kTextureIndices:Vector<Float> = new Vector<Float>([
+	private static var baselineTextureIndices:Vector<Float> = new Vector<Float>([
 		0.125, 0.375, 0.625, 0.875,
 		1, 0, 0, 0
 	]);
+	
+	private static var textureIndices:Vector<Float> = new Vector<Float>([
+		0.5, 1.5, 2.5, 3.5,
+		4.5, 5.5, 6.5, 7.5,
+		8.5, 9.5, 10.5, 11.5,
+		12.5, 13.5, 14.5, 15.5
+	]);
+	
+	private var _multiTexturingConstants:Vector<Float>;
 	
 	public function new()
 	{
@@ -233,6 +297,15 @@ class MultiTextureEffect extends MeshEffect
 		__isBaseline = Starling.current.profile == Context3DProfile.BASELINE ||
 					   Starling.current.profile == Context3DProfile.BASELINE_CONSTRAINED ||
 					   Starling.current.profile == Context3DProfile.BASELINE_EXTENDED;
+		
+		if (__isBaseline)
+		{
+			_multiTexturingConstants = baselineTextureIndices;
+		}
+		else
+		{
+			_multiTexturingConstants = textureIndices;
+		}
 	}
 	
 	override function get_programVariantName():UInt 
@@ -240,7 +313,8 @@ class MultiTextureEffect extends MeshEffect
 		var bits:UInt = super.get_programVariantName();
 		
 		for (i in 0...textures.length)
-			bits |= RenderUtil.getTextureVariantBits(textures[i]) << (4 * i + 4);
+			bits |= RenderUtil.getTextureVariantBits(textures[i]) << (i + 4);
+		
 		return bits;
 	}
 	
@@ -296,51 +370,78 @@ class MultiTextureEffect extends MeshEffect
 			}
 			else
 			{
-				if (length > 1)
-				{
-					fragmentShader.push("slt ft4, v2.xxxx, fc0");
-					fragmentShader.push("sub ft6, fc1.xxxx, ft4");
-					fragmentShader.push("min ft6.xyz, ft6.xyz, ft4.yzw");
-					fragmentShader.push("ifg ft4.x, fc0.z");
-					fragmentShader.push(FilterEffect.tex("ft5", "v0", 0, texture));
-					fragmentShader.push("eif");
-					fragmentShader.push("ifg ft6.x, fc0.z");
-					fragmentShader.push(FilterEffect.tex("ft5", "v0", 1, textures[0]));
-					fragmentShader.push("eif");
-					fragmentShader.push("ifg ft6.y, fc0.z");
-					fragmentShader.push(FilterEffect.tex("ft5", "v0", 2, textures[1]));
-					fragmentShader.push("eif");
-					
-					if (length > 2)
-					{
-						fragmentShader.push("ifg ft6.z, fc0.z");
-						fragmentShader.push(FilterEffect.tex("ft5", "v0", 3, textures[2]));
-						fragmentShader.push("eif");
-						
-						if (length > 3)
-						{
-							fragmentShader.push("ifg ft6.w, fc0.z");
-							fragmentShader.push(FilterEffect.tex("ft5", "v0", 4, textures[3]));
-							fragmentShader.push("eif");
-						}
-					}
-					
-				}
-				else
-				{
-					fragmentShader.push("ifl v2.x, fc0.x");
-					fragmentShader.push(FilterEffect.tex("ft5", "v0", 0, texture));
-					fragmentShader.push("els");
-					fragmentShader.push(FilterEffect.tex("ft5", "v0", 1, textures[0]));
-					fragmentShader.push("eif");
-				}
-				fragmentShader.push("mul oc, ft5, v1");	// multiply color with texel color
+				textures.unshift(_texture); // add base texture temporarily
+				multiTex(fragmentShader, textures);
+				textures.shift(); // remove base texture
+				
+                fragmentShader.push(
+                    "mul oc, ft0, v1" // multiply color with texel color
+                );
 			}
 			return Program.fromSource(vertexShader, fragmentShader.join("\n"),
-				   __isBaseline ? 1 : 2);
+				   __isBaseline ? 1 : 2, !__isBaseline);
 		}
 		
 		return super.createProgram();
+	}
+	
+	private function multiTex(data:Array<String>, textures:Vector<Texture>, numTextures:Int = 0, textureOffset:Int = 0, textureRegister:String = "ft0", textureIndexSource:String = "v2.x", constantsStartIndex:Int = 0):Void
+	{
+		if (numTextures == 0) numTextures = textures.length;
+		
+		if (numTextures <= 2)
+		{
+			if (numTextures == 2)
+			{
+				checkTexIndex(data, textureOffset, textureIndexSource, constantsStartIndex);
+				data[data.length] = RenderUtil.createAGALTexOperation(textureRegister, "v0", textureOffset, textures[textureOffset]);
+				data[data.length] = "els";
+				data[data.length] = RenderUtil.createAGALTexOperation(textureRegister, "v0", textureOffset + 1, textures[textureOffset + 1]);
+				data[data.length] = "eif";
+			}
+			else
+			{
+				data[data.length] = RenderUtil.createAGALTexOperation(textureRegister, "v0", textureOffset, textures[textureOffset]);
+			}
+		}
+		else
+		{
+			var halfNumTextures:Int = Math.ceil(numTextures / 2);
+			var remainingTextures:Int = numTextures - halfNumTextures;
+			
+			checkTexIndex(data, textureOffset + halfNumTextures - 1, textureIndexSource, constantsStartIndex);
+			multiTex(data, textures, halfNumTextures, textureOffset, textureRegister, textureIndexSource, constantsStartIndex);
+			data[data.length] = "els";
+			multiTex(data, textures, remainingTextures, textureOffset + halfNumTextures, textureRegister, textureIndexSource, constantsStartIndex);
+			data[data.length] = "eif";
+		}
+	}
+	
+	private function checkTexIndex(data:Array<String>, textureNum:Int, textureIndexSource:String, constantsStartIndex:Int):Void
+	{
+		var constantIndex:Int = constantsStartIndex + Math.floor(textureNum / 4);
+		var constantSubIndex:Int = textureNum % 4;
+		var constant:String;
+		
+		switch (constantSubIndex)
+		{
+			case 0 :
+				constant = " fc" + constantIndex + ".x";
+			
+			case 1 :
+				constant = " fc" + constantIndex + ".y";
+			
+			case 2 :
+				constant = " fc" + constantIndex + ".z";
+			
+			case 3 :
+				constant = " fc" + constantIndex + ".w";
+			
+			default :
+				throw new Error("incorrect constant sub index");
+		}
+		
+		data[data.length] = "ifl " + textureIndexSource + constant;
 	}
 	
 	override function beforeDraw(context:Context3D):Void 
@@ -360,7 +461,7 @@ class MultiTextureEffect extends MeshEffect
 			}
 			vertexFormat.setVertexBufferAt(3, vertexBuffer, "texture");
 			context.setProgramConstantsFromVector(Context3DProgramType.FRAGMENT,
-				0, kTextureIndices, length > 1 || __isBaseline ? -1 : 1);
+				0, _multiTexturingConstants, __isBaseline ? 2 : Math.ceil((length + 1) / 4));
 		}
 	}
 	
